5d9e56e262dfa3a9be4111ff072fc08bd00a371d
Diego Vega <diegosanti2006@hotmail.com>
2026-03-04 15:14:11 -0600

    feat: scaffold new Spring Boot backend application.

---
+++ backend/src/main/java/com/example/backend/EventNodeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventNodeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventNodeBackendApplication.class, args);
+	}
+
+}
